67332dfe0d19e3f63bc3ce2b3e1e6682123dbfe6
Adabala Phanindra Naidu <phanindaradabala@gmail.com>
2024-10-05 18:12:21 +0530

    version 2.0.2

--- content.js
+++ content.js
@@ -1,14 +1,13 @@
 // content.js
 window.addEventListener("load", () => {
-    // Retrieve the blocked ads count from background.js
-    chrome.runtime.sendMessage("getBlockedAdsCount", (response) => {
-      if (response) {
-        const blockedCount = response.count;
-        const countElement = document.getElementById("blockedCount");
-        if (countElement) {
-          countElement.textContent = blockedCount; // Update the UI with the blocked count
-        }
+  // Retrieve the blocked ads count from background.js
+  chrome.runtime.sendMessage("getBlockedAdsCount", (response) => {
+    if (response) {
+      const blockedCount = response.count;
+      const countElement = document.getElementById("blockedCount");
+      if (countElement) {
+        countElement.textContent = blockedCount; // Update the UI with the blocked count
       }
-    });
+    }
   });
-  
+});
